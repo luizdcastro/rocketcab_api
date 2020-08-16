167ea9d5ca69d33309f97a422c35772257c23a22
unknown <luizf.dcastro@gmail.com>
2020-08-16 18:06:27 -0300

    email smtp ip config

--- utils/email.js
+++ utils/email.js
@@ -5,8 +5,8 @@ const sendEmail = async (options) => {
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     auth: {
-      user: 'luizdcastro',
-      pass: 'Fortuna@2020',
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
