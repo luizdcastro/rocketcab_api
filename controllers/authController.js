0dec278a9513a2ebb2dabfa7f7a2a9187712d4bd
unknown <luizf.dcastro@gmail.com>
2020-08-15 22:15:24 -0300

    login error message fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -79,7 +79,7 @@ exports.login = catchAsync(async (req, res, next) => {
   const user = await User.findOne({ email }).select('+password');
 
   if (!user || !(await user.correctPassword(password, user.password))) {
-    return next(new AppError('Incorrect email or password'), 401);
+    next(new AppError('Incorrect email or password', 401));
   }
 
   createSendToken(user, 200, res);
