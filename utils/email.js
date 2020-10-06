59ffe2cd5285d5b85ec6a6d7dc9477b30adc0f76
unknown <luizf.dcastro@gmail.com>
2020-10-06 11:59:47 -0300

    payment integration

--- utils/email.js
+++ utils/email.js
@@ -11,7 +11,7 @@ const sendEmail = async (options) => {
   });
 
   const mailOptions = {
-    from: 'Rocketcab <text@sd1.emailtesthub.com>',
+    from: 'Luiz <text@sd1.emailtesthub.com>',
     to: options.email,
     subject: options.subject,
     text: options.message,
