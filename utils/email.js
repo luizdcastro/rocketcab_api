8fb1cd9a3d80c1808a0108eae6c186ce0a9b9e05
unknown <luizf.dcastro@gmail.com>
2020-10-12 12:15:20 -0300

    email sender config

--- utils/email.js
+++ utils/email.js
@@ -11,7 +11,7 @@ const sendEmail = async (options) => {
   });
 
   const mailOptions = {
-    from: 'Luiz <text@sd1.emailtesthub.com>',
+    from: 'Rocketcab <noreply@rocketcab.com.br>',
     to: options.email,
     subject: options.subject,
     text: options.message,
